e8cd4745f0caf36fe97ffcbca4352d50d5669b8e
Daniel Angosto Martinez <daniel.angosto.martinez@gmail.com>
2024-10-24 18:37:03 +0200

    Added mock data

--- src/app/modules/shared/interceptors/auth.interceptor.spec.ts
+++ src/app/modules/shared/interceptors/auth.interceptor.spec.ts
@@ -15,10 +15,11 @@ describe('AuthInterceptor', () => {
   });
 
   it('should add Authorization header if token is present', () => {
-    const token = 'mock-token';
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5YzdhNzM2Yi0yNmE5LTQxMDAtYTkzNi04Y2E5ODc5ODU3ZjMiLCJlbWFpbCI6InRlc3R1c2VyQGV4YW1wbGUuY29tIiwiYXVkIjoiYWNjb3VudCIsImlzcyI6IkF0d29yayIsImV4cCI6MTcyOTc2NTQyNCwiaWF0IjoxNzI5NzYxODI0LCJuYmYiOjE3Mjk3NjE4MjR9.wCJWGLU37avTdlrYwRI8QJYtAOWa3oN9i5cpavQTUfg';
     spyOn(localStorage, 'getItem').and.returnValue(token);
 
-    const httpRequest = new HttpRequest('GET', '/test-url');
+    const httpRequest = new HttpRequest('GET', '/Shared/Countries');
     const httpHandler: HttpHandler = {
       handle: jasmine
         .createSpy('handle')
@@ -38,7 +39,7 @@ describe('AuthInterceptor', () => {
   it('should not add Authorization header if no token is present', () => {
     spyOn(localStorage, 'getItem').and.returnValue(null);
 
-    const httpRequest = new HttpRequest('GET', '/test-url');
+    const httpRequest = new HttpRequest('GET', '/Shared/Countries');
     const httpHandler: HttpHandler = {
       handle: jasmine
         .createSpy('handle')
